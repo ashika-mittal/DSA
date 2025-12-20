8e156029db0f1c64c97224edb2b55f02fc726903
ashika-mittal <ashika.mittal05@gmail.com>
2025-12-20 18:42:36 +0530

    basics of bit manipulation

--- 15- Bit Manipulation/BitwiseOperators.cpp
+++ 15- Bit Manipulation/BitwiseOperators.cpp
@@ -7,4 +7,13 @@ using namespace std;
 
 int main(){
     cout<< (3 & 5) <<endl;
+    cout<< (3 | 5) <<endl;
+    cout<< (3 ^ 5) <<endl;
+    cout<< ~6 <<endl;
+    cout<< ~4 <<endl;
+    cout<< ~0 <<endl;
+    cout<< ~1 <<endl;
+    
+    cout<< (7<<2) <<endl;
+    cout<< (7>>2) <<endl;
 }
