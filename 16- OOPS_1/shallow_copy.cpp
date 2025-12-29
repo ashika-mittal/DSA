ba69671c4bb5ab4c194dc3d1fd987130cec31d1a
ashika-mittal <ashika.mittal05@gmail.com>
2025-12-29 17:51:11 +0530

    deep copy and shallow copy

---
+++ 16- OOPS_1/shallow_copy.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Car {
+    public:   
+        string name;
+        string color;
+        int* mileage;
+
+        Car(string name, string color){
+            this->name= name;
+            this->color = color;
+            mileage = new int;
+            *mileage = 250;
+        }
+
+        // //custom copy constructor
+        // Car(Car &original){
+        //     cout<< "copy original to new ... " <<endl;
+        //     name = original.name;
+        //     color = original.color;
+        //     mileage = original.mileage;
+        // }
+};
+
+
+int main(){
+  
+    Car c1 ("innova " , "white"); 
+    Car c2(c1); //custom copy constructor wll be called.
+
+    cout<< "carname : " <<c1.name <<endl;
+    cout<<"car color : " <<c1.color <<endl;
+    cout <<"mileage : " <<c1.mileage <<endl;
+    cout <<"mileage : " << *c1.mileage <<endl;
+    cout<< endl;
+    cout<< "carname : " << c2.name <<endl;
+    cout<<"car color : " << c2.color <<endl;
+    cout <<"mileage : " <<c2.mileage <<endl;
+    cout <<"mileage : " << *c2.mileage <<endl;
+    
+    
+
+}
