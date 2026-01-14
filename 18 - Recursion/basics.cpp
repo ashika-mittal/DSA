87ab5b46f67e2bce89ab8f3c77b54a6a56bdd24d
ashika-mittal <ashika.mittal05@gmail.com>
2026-01-14 19:20:07 +0530

    recursion basics

---
+++ 18 - Recursion/basics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+ //infinite recursive func-> no base case.   
+        // void func(){
+        //     cout<<"calling function..." <<endl;
+        //     func();
+        // }
+
+int main(){
+    func();
+}
