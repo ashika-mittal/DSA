686e1b833ba11a61ab4c0f2adac745cc9fa51955
ashika-mittal <ashika.mittal05@gmail.com>
2026-07-04 00:27:17 +0530

    STL Queue 1

---
+++ 28 - Queue/Queue_in_STL.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    cout<<"Front element: "<<q.front()<<endl;
+    cout<<"Size of queue: "<<q.size()<<endl;
+
+    q.pop();
+    cout<<"Front element after pop: "<<q.front()<<endl;
+    cout<<"Size of queue after pop: "<<q.size()<<endl;
+
+    return 0;
+}
