c9f09094305eb05e71bb759f31ba3a5538f7f8ff
ashika-mittal <ashika.mittal05@gmail.com>
2026-07-06 16:26:55 +0530

    indent proper

--- 28 - Queue/Queue_using_LL.cpp
+++ 28 - Queue/Queue_using_LL.cpp
@@ -3,61 +3,61 @@ using namespace std;
 
 class Node{
     public:
-    int data;
-    Node* next;
+        int data;
+        Node* next;
 
-    Node(int data){
-        this->data = data;
-        this->next = NULL;
-    }
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
 };
 
 class Queue{
     Node *head, *tail;
 
     public:
-    Queue(){
-        head = NULL;
-        tail = NULL;
-    }
-
-    void push(int data){
-        Node* newNode = new Node(data);
-        if(head == NULL){
-            head = newNode;
-            tail = newNode;
-        }else{
-            tail->next = newNode;
-            tail = newNode;
-        }
-
-    }
-
-    void pop(){
-        if(empty()){
-            cout<<"Queue is empty \n"<<endl;
-            return;
-        }
-        Node* temp = head;
-        head = head->next;
-        if(head == NULL){
+        Queue(){
+            head = NULL;
             tail = NULL;
         }
-        delete temp;
-    }
 
-    int front(){
-        if(empty()){
-            cout<<"Queue is empty \n"<<endl;
-            return -1;
-        }else{
-            return head->data;
+        void push(int data){
+            Node* newNode = new Node(data);
+            if(head == NULL){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+
         }
-    }
 
-    bool empty(){
-        return head == NULL;
-    }
+        void pop(){
+            if(empty()){
+                cout<<"Queue is empty \n"<<endl;
+                return;
+            }
+            Node* temp = head;
+            head = head->next;
+            if(head == NULL){
+                tail = NULL;
+            }
+            delete temp;
+        }
+
+        int front(){
+            if(empty()){
+                cout<<"Queue is empty \n"<<endl;
+                return -1;
+            }else{
+                return head->data;
+            }
+        }
+
+        bool empty(){
+            return head == NULL;
+        }
 
 };
 
