51f14d7321002baf0233fa5284d147ec1b689b26
ashika-mittal <ashika.mittal05@gmail.com>
2026-07-18 03:13:52 +0530

    pair in cpp

---
+++ 29- Greedy Algorithms/pairs.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    //pairs in C++ STL : pair is a container that holds two values of same/ different data types
+
+    //sorting in pairs
+    vector<int> start = {0,1,2};
+    vector<int> end = {9,2,4};
+
+    vector<pair<int,int>> v(3, make_pair(0,0));
+
+    for(int i=0; i<start.size(); i++){
+        //v.push_back({end[i], start[i]});
+        v[i].first = end[i];
+        v[i].second = start[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<v.size(); i++){
+        end[i] = v[i].first;
+        start[i] = v[i].second;
+    }
+
+    for(int i=0; i<v.size(); i++){
+        cout<<start[i]<<" "<<end[i]<<endl;
+    }
+
+
+
+
+}
